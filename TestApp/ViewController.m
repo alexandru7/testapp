770b537b4a0d0372817d41d1de405e943b4b0b20
alexandru7 <alexandru7@gmail.com>
2017-04-02 16:01:08 -0400

    - added initial logic for handling taps on the button and the circle

--- TestApp/ViewController.m
+++ TestApp/ViewController.m
@@ -8,7 +8,13 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#define MINIMUM_PRESS_DURATION 1.0
+
+@interface ViewController () {
+	UILongPressGestureRecognizer *longPressRecognizer;
+	UITapGestureRecognizer *tapRecognizer;
+	NSDate *startTime;
+}
 
 @end
 
@@ -17,9 +23,46 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	
-	self.circleView.backgroundColor = [UIColor blueColor];
+	longPressRecognizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(longPressAction:)];
+	[longPressRecognizer setMinimumPressDuration:MINIMUM_PRESS_DURATION];
+	[self.actionButton addGestureRecognizer:longPressRecognizer];
+	
+	tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(circleTapAction:)];
+	
 	self.circleView.layer.cornerRadius = self.circleView.frame.size.width / 2;
 	self.circleView.layer.shouldRasterize = YES;
+	
+	[self setCircleViewToInitialState];
+}
+
+- (void)longPressAction:(UILongPressGestureRecognizer *)longPressGestureRecognizer {
+	//make sure the delay si between 3 to 7 seconds
+	long timerDelay = arc4random_uniform(4) + 3;
+	
+	[NSTimer timerWithTimeInterval:timerDelay repeats:NO block:^(NSTimer * _Nonnull timer) {
+		[self setCircleViewToInteractiveState];
+		self.instructionsLabel.text = NSLocalizedString(@"Tap the green circle", nil);
+		
+		startTime = [NSDate date];
+	}];
+}
+
+- (void)circleTapAction:(UITapGestureRecognizer *)tapGestureRecognizer {
+	
+}
+
+- (void)setCircleViewToInitialState {
+	self.circleView.backgroundColor = [UIColor blueColor];
+	self.circleView.userInteractionEnabled = NO;
+	[self.circleView removeGestureRecognizer:tapRecognizer];
+	
+	startTime = nil;
+}
+
+- (void)setCircleViewToInteractiveState {
+	self.circleView.backgroundColor = [UIColor greenColor];
+	self.circleView.userInteractionEnabled = YES;
+	[self.circleView addGestureRecognizer:tapRecognizer];
 }
 
 
